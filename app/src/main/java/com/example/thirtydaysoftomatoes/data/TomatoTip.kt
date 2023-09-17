cb3195f96e500c78bb7ae74bcccd984af94614ee
entropia <marta9kut@gmail.com>
2023-09-17 15:35:35 +0200

    added string resources

--- app/src/main/java/com/example/thirtydaysoftomatoes/data/TomatoTip.kt
+++ app/src/main/java/com/example/thirtydaysoftomatoes/data/TomatoTip.kt
@@ -2,9 +2,21 @@ package com.example.thirtydaysoftomatoes.data
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import com.example.thirtydaysoftomatoes.R
 
 data class TomatoTip(
     @StringRes val title: Int,
     @DrawableRes val image: Int,
     @StringRes val description:Int
 )
+
+
+object TomatoTipsRepository{
+    val tomatoTips= listOf(
+        TomatoTip(
+            title = R.string.tip1_title,
+            image=0,
+            description = R.string.tip1_description
+        )
+    )
+}
