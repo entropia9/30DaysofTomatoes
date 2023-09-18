0cb206c126248344902baf7cfa6e560b030f60c7
entropia <marta9kut@gmail.com>
2023-09-18 19:26:16 +0200

    first draft of Tomato Tip Card

--- app/src/main/java/com/example/thirtydaysoftomatoes/MainActivity.kt
+++ app/src/main/java/com/example/thirtydaysoftomatoes/MainActivity.kt
@@ -4,15 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.thirtydaysoftomatoes.data.TomatoTip
 import com.example.thirtydaysoftomatoes.ui.theme.ThirtyDaysOfTomatoesTheme
@@ -44,21 +58,94 @@ fun TomatoTipList(modifier: Modifier = Modifier) {
 @Composable
 fun TomatoTipCard(
     tomatoTip: TomatoTip,
+    dayNumber: Int,
     modifier: Modifier = Modifier
 ) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
     Card(modifier = modifier) {
-        Text(
-            text = stringResource(id = tomatoTip.title)
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(
+                top = dimensionResource(id = R.dimen.padding_medium),
+                end = dimensionResource(id = R.dimen.padding_medium),
+                start = dimensionResource(id = R.dimen.padding_medium)
+            )
+        ) {
+
+            Text(
+                text = stringResource(id = R.string.day) + " $dayNumber",
+                modifier = Modifier.weight(1f)
+            )
+
+
+            Image(painter = painterResource(id = tomatoTip.image), contentDescription = null)
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = dimensionResource(id = R.dimen.padding_medium))
+        ) {
+            Text(
+                text = stringResource(id = tomatoTip.title),
+                textAlign = TextAlign.End,
+                modifier = modifier.weight(1f)
+            )
+            ExpandButton(
+                expanded = expanded,
+                onClick = { expanded = !expanded },
+                modifier
+
+                    .padding(end = dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+
+        if (expanded) {
+            Text(
+                text = stringResource(id = tomatoTip.description),
+                textAlign = TextAlign.Justify,
+                modifier = Modifier.padding(
+                    start = dimensionResource(id = R.dimen.padding_medium),
+                    end = dimensionResource(id = R.dimen.padding_medium),
+                    bottom = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+        }
+
+
+    }
+}
+
+@Composable
+fun ExpandButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = if (expanded) ImageVector.vectorResource(id = R.drawable.nutrition_24px) else ImageVector.vectorResource(
+                id = R.drawable.psychiatry_24px
+            ),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.secondary
         )
-        Image(painter = painterResource(id = tomatoTip.image), contentDescription = null)
-        Text(
-            text = stringResource(id = tomatoTip.description)
-        )
+
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun TomatoTipCardPreview() {
-
+    val tomatoTip = TomatoTip(
+        title = R.string.tip1_title,
+        image = R.drawable.ic_launcher_foreground,
+        description = R.string.tip1_description
+    )
+    TomatoTipCard(tomatoTip = tomatoTip, dayNumber = 1)
 }
--- app/src/main/java/com/example/thirtydaysoftomatoes/data/TomatoTip.kt
+++ app/src/main/java/com/example/thirtydaysoftomatoes/data/TomatoTip.kt
@@ -17,6 +17,151 @@ object TomatoTipsRepository{
             title = R.string.tip1_title,
             image=0,
             description = R.string.tip1_description
+        ),
+        TomatoTip(
+            title = R.string.tip2_title,
+            image=0,
+            description = R.string.tip2_description
+        ),
+        TomatoTip(
+            title = R.string.tip3_title,
+            image=0,
+            description = R.string.tip3_description
+        ),
+        TomatoTip(
+            title = R.string.tip4_title,
+            image=0,
+            description = R.string.tip4_description
+        ),
+        TomatoTip(
+            title = R.string.tip5_title,
+            image=0,
+            description = R.string.tip5_description
+        ),
+        TomatoTip(
+            title = R.string.tip6_title,
+            image=0,
+            description = R.string.tip6_description
+        ),
+        TomatoTip(
+            title = R.string.tip7_title,
+            image=0,
+            description = R.string.tip7_description
+        ),
+        TomatoTip(
+            title = R.string.tip8_title,
+            image=0,
+            description = R.string.tip8_description
+        ),
+        TomatoTip(
+            title = R.string.tip9_title,
+            image=0,
+            description = R.string.tip9_description
+        ),
+        TomatoTip(
+            title = R.string.tip10_title,
+            image=0,
+            description = R.string.tip10_description
+        ),
+        TomatoTip(
+            title = R.string.tip11_title,
+            image=0,
+            description = R.string.tip11_description
+        ),
+        TomatoTip(
+            title = R.string.tip12_title,
+            image=0,
+            description = R.string.tip12_description
+        ),
+        TomatoTip(
+            title = R.string.tip13_title,
+            image=0,
+            description = R.string.tip13_description
+        ),
+        TomatoTip(
+            title = R.string.tip14_title,
+            image=0,
+            description = R.string.tip14_description
+        ),
+        TomatoTip(
+            title = R.string.tip15_title,
+            image=0,
+            description = R.string.tip15_description
+        ),
+        TomatoTip(
+            title = R.string.tip16_title,
+            image=0,
+            description = R.string.tip16_description
+        ),
+        TomatoTip(
+            title = R.string.tip17_title,
+            image=0,
+            description = R.string.tip17_description
+        ),
+        TomatoTip(
+            title = R.string.tip18_title,
+            image=0,
+            description = R.string.tip18_description
+        ),
+        TomatoTip(
+            title = R.string.tip19_title,
+            image=0,
+            description = R.string.tip19_description
+        ),
+        TomatoTip(
+            title = R.string.tip20_title,
+            image=0,
+            description = R.string.tip20_description
+        ),
+        TomatoTip(
+            title = R.string.tip21_title,
+            image=0,
+            description = R.string.tip21_description
+        ),
+        TomatoTip(
+            title = R.string.tip22_title,
+            image=0,
+            description = R.string.tip22_description
+        ),
+        TomatoTip(
+            title = R.string.tip23_title,
+            image=0,
+            description = R.string.tip23_description
+        ),
+        TomatoTip(
+            title = R.string.tip24_title,
+            image=0,
+            description = R.string.tip24_description
+        ),
+        TomatoTip(
+            title = R.string.tip25_title,
+            image=0,
+            description = R.string.tip25_description
+        ),
+        TomatoTip(
+            title = R.string.tip26_title,
+            image=0,
+            description = R.string.tip26_description
+        ),
+        TomatoTip(
+            title = R.string.tip27_title,
+            image=0,
+            description = R.string.tip27_description
+        ),
+        TomatoTip(
+            title = R.string.tip28_title,
+            image=0,
+            description = R.string.tip28_description
+        ),
+        TomatoTip(
+            title = R.string.tip29_title,
+            image=0,
+            description = R.string.tip29_description
+        ),
+        TomatoTip(
+            title = R.string.tip30_title,
+            image=0,
+            description = R.string.tip30_description
         )
     )
 }
